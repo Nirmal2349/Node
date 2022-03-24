1cced4439a6932d4b20c4aa884ec5990d3b9ad67
Nirmal2349 <nirmalrajr35@gmail.com>
2022-03-24 18:48:46 +0530

    to read a file content via nodejs

--- file.js
+++ file.js
@@ -1,13 +1,34 @@
-const fs = require("fs");
+/// to write and create file
 
-const quote2 = "earn better is good for all";
+// const fs = require("fs");
 
-// fs.writeFile("./backup/hai.html", quote, (err) => {
+// const quote2 = "earn better is good for all";
+
+// to create a single file via node.js
+
+// fs.writeFile("./hai.html", quote2, (err) => {
 //   console.log("completed writing !!!");
 // });
 
-for (let i = 1; i <= 10; i++) {
-  fs.writeFile(`./backup/text-${i}.html`, quote2, (err) => {
-    console.log("Completed writing !!! 👍");
-  });
-}
+// to create a n-number of file via node.js
+
+// const fs = require("fs");
+
+// const quote2 = "earn better is good for all";
+
+// for (let i = 1; i <= 10; i++) {
+//   fs.writeFile(`./backup/text-${i}.html`, quote2, (err) => {
+//     console.log("Completed writing !!! 👍");
+//   });
+// }
+
+/// to read file
+
+const fs = require("fs");
+
+fs.readFile("./dude.txt", "utf8", (err, content) => {
+  if (err) {
+    console.log("file not found ", err);
+  }
+  console.log("quote", content);
+});
